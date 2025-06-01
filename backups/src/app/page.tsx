e8c9a22454d65b7c5b7dc637f478e5b9c72ac8c5
devenspear <deven@spear.limited>
2025-06-01 13:55:07 -0400

    Fix import paths in backups/src/app/page.tsx

--- backups/src/app/page.tsx
+++ backups/src/app/page.tsx
@@ -1,12 +1,12 @@
-import HeroSection from "../../components/HeroSection";
-import FastLaneSection from "../../components/FastLaneSection";
-import NewsAndDisruptionSection from "../../components/NewsAndDisruptionSection";
-import VideoInterviewsSection from "../../components/VideoInterviewsSection";
-import ResourceLibrarySection from "../../components/ResourceLibrarySection";
-import ThoughtLeadersSection from "../../components/ThoughtLeadersSection";
-import LearningResourcesSection from "../../components/LearningResourcesSection";
-import AboutWithSubscription from "../../components/AboutWithSubscription";
-import Footer from "../../components/Footer";
+import HeroSection from "../../../components/HeroSection";
+import FastLaneSection from "../../../components/FastLaneSection";
+import NewsAndDisruptionSection from "../../../components/NewsAndDisruptionSection";
+import VideoInterviewsSection from "../../../components/VideoInterviewsSection";
+import ResourceLibrarySection from "../../../components/ResourceLibrarySection";
+import ThoughtLeadersSection from "../../../components/ThoughtLeadersSection";
+import LearningResourcesSection from "../../../components/LearningResourcesSection";
+import AboutWithSubscription from "../../../components/AboutWithSubscription";
+import Footer from "../../../components/Footer";
 
 export default function Home() {
   return (
